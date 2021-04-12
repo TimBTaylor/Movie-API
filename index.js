5a9b82a0ad34aa32a5d42fe0a9682c9202e4b6d6
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-04-12 12:33:52 -0400

    completion of achievement 2.5 Rest & API endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+app.use(express.static("public"));
+
 app.use(express.static("public/documentation.html"));
 
 app.use(morgan("common"));
@@ -22,8 +24,36 @@ app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
-app.get("/", (req, res) => {
-  res.send("This is my practice code");
+app.get("/movies/:title", (req, res) => {
+  res.send("successful GET request on specified movie info");
+});
+
+app.get("/movies/:title/genre", (req, res) => {
+  res.send("successful GET request on movie genre");
+});
+
+app.get("/movies/director/:name", (req, res) => {
+  res.send("successful GET request on director information");
+});
+
+app.post("/users", (req, res) => {
+  res.send("successful POST request on creating user");
+});
+
+app.put("/users/:username/:newusername", (req, res) => {
+  res.send("successful PUT request on changing username");
+});
+
+app.post("/users/:username/movies/:movieID", (req, res) => {
+  res.send("successful POST request on adding movie");
+});
+
+app.delete("/users/:username/movies/:movieID", (req, res) => {
+  res.send("successful DELETE request on removing a movie");
+});
+
+app.delete("/users/:username", (req, res) => {
+  res.send("successful DELETE request on users account");
 });
 
 app.listen(8080, () => {
