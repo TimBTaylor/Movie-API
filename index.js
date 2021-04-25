63998955d7f07b3ecd3c015097cbfeb2dfafbdb0
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-04-25 18:36:13 -0400

    added movies/users collection files and added a seperate mongoose.connect

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const cors = require("cors");
 const { check, validationResult } = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
-
+/*
 mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+*/
+
+mongoose.connect("process.env.CONNECTION_URI", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
