f522f9efd579c631115d545adff4dcc8e2727132
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-04-26 16:07:48 -0400

    fixed error in code

--- passport.js
+++ passport.js
@@ -14,7 +14,7 @@ passport.use(
       passwordField: "Password"
     },
     (username, password, callback) => {
-      console.log(username + "  " + password);
+      console.log(username + " " + password);
       Users.findOne({ Username: username }, (error, user) => {
         if (error) {
           console.log(error);
@@ -23,14 +23,13 @@ passport.use(
 
         if (!user) {
           console.log("incorrect username");
-          return callback(null, false, {
-            message: "Incorrect username or password."
-          });
+          return callback(null, false, { message: "Incorrect username." });
+        }
 
         if (!user.validatePassword(password)) {
-      console.log('incorrect password');
-      return callback(null, false, {message: 'Incorrect password.'});
-    }
+          console.log("incorrect password");
+          return callback(null, false, { message: "Incorrect password." });
+        }
 
         console.log("finished");
         return callback(null, user);
