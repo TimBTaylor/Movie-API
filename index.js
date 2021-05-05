874fb14e88f85868a81e97bf92a02c6e2c9a089e
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-05-05 04:18:44 -0400

    reverted changes

--- index.js
+++ index.js
@@ -60,16 +60,19 @@ app.get("/", (req, res) => {
 });
 
 //GET request to return all movies and information about them
-app.get("/movies", function (req, res) {
-  Movies.find()
-    .then(function (movies) {
-      res.status(201).json(movies);
-    })
-    .catch(function (error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    });
-});
+app.get(
+  "/movies",
+  (req, res) => {
+    Movies.find()
+      .then(movies => {
+        res.status(201).json(movies);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //GET request to recieve infromation about a movie via the title
 app.get(
