b75432c1ecfec8d930b73c236466ca50df9da982
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-05-21 18:33:56 -0400

    added netlify origins

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static("public/documentation.html"));
 
 app.use(morgan("common"));
 
-let allowedOrigins = ["https://localhost:8080", "http://testsite.com", "http://localhost:1234", "https://timsmyflix.herokuapp.com"];
+let allowedOrigins = ["https://localhost:8080", "http://testsite.com", "http://localhost:1234", "https://timsmyflix.herokuapp.com", "https://timsmyflix.netlify.app"];
 
 app.use(
   cors({
